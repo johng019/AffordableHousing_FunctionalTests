92fff83ff5209ccadac84d0a0add27f8ba2855ad
Gary Johnston <garyljohnstonjr@gmail.com>
2022-07-30 06:40:09 -0400

    Add Production link for testing

--- Affordable-Housing-Tests/src/sampleapp/IGlobalConstants.java
+++ Affordable-Housing-Tests/src/sampleapp/IGlobalConstants.java
@@ -2,5 +2,6 @@ package sampleapp;
 
 public interface IGlobalConstants {
     String site = "https://glowing-semolina-c74dd9.netlify.app/";
+    //String site = "https://genuine-ganache-d769f7.netlify.app/";
     String local = "http://localhost:4200";
 }
